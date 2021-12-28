20e232be2db8e6c3b1c8a121d4fa53080c6c6219
Collins Lator <84387829+jancoza@users.noreply.github.com>
2021-12-28 22:36:27 +0100

    first commit

---
+++ cac.js
@@ -0,0 +1,11 @@
+// "use strict";
+
+let change = document.getElementById("navLinks");
+
+function showMenu() {
+  change.style.right = "0";
+}
+
+function hideMenu() {
+  change.style.right = "-200px";
+}
